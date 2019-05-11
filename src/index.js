468ddcc6832c37c3b3c4a46d3bcbfbf3dcacaed7
Silviu Avram <siavram@microsoft.com>
2019-05-11 16:13:37 +0300

    added example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,71 @@
 import ReactDOM from 'react-dom'
+import React from 'react'
+import useDownshiftSingleSelection from './downshiftSingleSelection'
 
-const title = 'My Minimal React Webpack Babel Setup'
+const options = [
+  'Neptunium',
+  'Plutonium',
+  'Americium',
+  'Curium',
+  'Berkelium',
+  'Californium',
+  'Einsteinium',
+  'Fermium',
+  'Mendelevium',
+  'Nobelium',
+  'Lawrencium',
+  'Rutherfordium',
+  'Dubnium',
+  'Seaborgium',
+  'Bohrium',
+  'Hassium',
+  'Meitnerium',
+  'Darmstadtium',
+  'Roentgenium',
+  'Copernicium',
+  'Nihonium',
+  'Flerovium',
+  'Moscovium',
+  'Livermorium',
+  'Tennessine',
+  'Oganesson',
+]
+
+const Dropdown = () => {
+  const {
+    isOpen,
+    getTriggerButtonProps,
+    getLabelProps,
+    getMenuProps,
+    highlightedIndex,
+    getItemProps,
+  } = useDownshiftSingleSelection({ initialIsOpen: true })
+  return (
+    <div id="exp_wrapper">
+      <label {...getLabelProps()}>
+        Choose an element:
+      </label>
+      <button
+        {...getTriggerButtonProps()}>
+        {options[0]}
+      </button>
+      <ul {...getMenuProps()}>
+        {isOpen && options.map((option, index) => (
+          <li
+            style={highlightedIndex === index ? { backgroundColor: 'blue' } : {}}
+            key={`${option}${index}`}
+            {...getItemProps({ item: option, index })}
+          >
+            {option}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <Dropdown />,
   document.getElementById('app'),
 )
 
