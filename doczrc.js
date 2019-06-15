943ac8abce1ef9836b1783f6083a02e776d5f4a3
silviuavram <silviu.avram91@gmail.com>
2019-06-15 15:51:06 +0200

    changed the menu order in docz

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,18 @@
 export default {
   title: 'Downshift Hooks',
   description: 'Use Downshift Accessibility logic in your widgets.',
+  menu: [
+    {
+      name: 'Getting started',
+      menu: [
+        'Downshift hooks',
+      ],
+    },
+    {
+      name: 'Dropdown',
+      menu: [
+        'Selection',
+      ],
+    },
+  ],
 }
