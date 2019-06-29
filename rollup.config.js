d1b9945ba507293f93257065147f707cabc8df9c
silviuavram <silviu.avram91@gmail.com>
2019-06-29 11:33:01 +0200

    finished rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,23 @@ const commonjs = require('rollup-plugin-commonjs')
 const config = require('kcd-scripts/dist/config/rollup.config.js')
 
 const cjsPluginIndex = config.plugins.findIndex(
-	plugin => plugin.name === 'commonjs',
+  plugin => plugin.name === 'commonjs',
 )
 config.plugins[cjsPluginIndex] = commonjs({
-	include: 'node_modules/**',
-	namedExports: {
-		'node_modules/react/index.js': [
-			'useReducer',
-			'useEffect',
-			'useRef',
-		],
-	},
+  include: 'node_modules/**',
+  namedExports: {
+    'node_modules/react/index.js': [
+      'useReducer',
+      'useEffect',
+      'useRef',
+    ],
+    'node_modules/lodash/lodash.js': [
+      'findIndex',
+    ],
+    'node_modules/keyboard-key/src/keyboardKey.js': [
+      'getKey',
+    ],
+  },
 })
 
 module.exports = config
